26d4be3596d67f2c403255cd5df4239fa83cd2bc
turaa <ermiaskelbessa@gmail.com>
2022-09-04 17:00:52 -0700

    remove duplicate

--- scripts/load_feeds.js
+++ scripts/load_feeds.js
@@ -1,5 +1,14 @@
 // load feed u say
+
 chrome.storage.local.get(['vids'], function(result) {
-    console.log(result);
-    document.getElementById("feed-view").innerHTML = result.vids
+    var duplicate = []
+    for(let videoLink of result.vids){
+        if(!duplicate.includes(videoLink)){
+            var node = document.createElement("LI");
+            var textnode = document.createTextNode(videoLink);
+            node.appendChild(textnode); 
+            document.getElementById("feed-view").appendChild(node);
+            duplicate.push(videoLink)
+        }
+    }
 });
